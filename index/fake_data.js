4c7033880eb59d180a64dfe0567597a7d9e30b60
mikeonslow <mikeonslow@gmail.com>
2018-02-22 10:51:10 -0500

    API for Dashboard, Widget and Chart Data is complete

--- index/fake_data.js
+++ index/fake_data.js
@@ -4,19 +4,60 @@ const faker = require("faker");
 const R = require("ramda");
 
 var MakeData = function() {
+  var widgetCount = 1; // TODO Refactor to more functional style with something like http://ramdajs.com/docs/#addIndex, ran out of time :[
+
+  const dashboardMax = 10;
+  const widgetsPerDashboard = 10;
+
+  const dashboards = generateDashboards(dashboardMax);
+
+  var widgetMapper = R.compose(
+    R.map(n => {
+      return generateWidgets(widgetsPerDashboard, n.id);
+    })
+  );
+
+  const widgets = R.flatten(widgetMapper(dashboards));
+
+  const charts = R.map(n => {
+    return generateChart(n.id);
+  }, widgets);
+
+  function generateDashboards(count) {
+    const start = 1;
+    return R.map(n => {
+      return { id: n, label: `Dashboard ${n}` };
+    }, R.range(start, start + count));
+  }
+
+  function generateWidgets(count, dashboardId) {
+    return R.map(n => {
+      const id = widgetCount++;
+      return {
+        id: id,
+        dashboardId: dashboardId,
+        label: `Widget ${id}`
+      };
+    }, R.range(0, count));
+  }
+
+  function generateChart(widgetId) {
+    return { id: widgetId, data: generateFakeChartData(8) };
+  }
+
+  function generateFakeChartData(max) {
+    return R.map(n => {
+      return [faker.name.findName(), faker.random.number({ min: 5, max: 50 })];
+    }, R.range(1, max));
+  }
+
   return {
     api: {
-      dashboards: generateDashboards(),
-      widgets: []
+      dashboards: dashboards,
+      widgets: widgets,
+      charts: charts
     }
   };
 };
-
-function generateDashboards(count) {
-  const start = 100001;
-  return R.map(n => {
-    return { id: n };
-  }, R.range(start, start + count));
-}
 
 module.exports = MakeData;
