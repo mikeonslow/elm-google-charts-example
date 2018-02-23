9414a985c10d9274568065e36358ddf4d95e2922
mikeonslow <mikeonslow@gmail.com>
2018-02-23 07:17:27 -0500

    Added encoders to Data.Chart

--- src/elm/Data/Chart.elm
+++ src/elm/Data/Chart.elm
@@ -2,10 +2,12 @@ module Data.Chart exposing (..)
 
 import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Pipeline as Pipeline exposing (decode, optional, required)
+import Json.Encode as Encode exposing (Value)
+import Util exposing ((=>))
 
 
 type alias Data =
-    { id : Float, data : List (List Point) }
+    { id : Float, points : List (List Point) }
 
 
 type alias Points =
@@ -20,14 +22,38 @@ decoder : Decoder Data
 decoder =
     decode Data
         |> required "id" Decode.float
-        |> required "data" (Decode.list <| Decode.list <| arrayAsTuple2 Decode.string Decode.float)
+        |> required "points" (Decode.list <| Decode.list <| tuple2Decoder Decode.string Decode.float)
 
 
-arrayAsTuple2 : Decoder a -> Decoder b -> Decoder ( a, b )
-arrayAsTuple2 a b =
+tuple2Decoder : Decoder a -> Decoder b -> Decoder ( a, b )
+tuple2Decoder a b =
     Decode.index 0 a
         |> Decode.andThen
             (\a ->
                 Decode.index 1 b
                     |> Decode.andThen (\b -> Decode.succeed ( a, b ))
             )
+
+
+encoder data =
+    Encode.object
+        [ "id" => Encode.float data.id
+        , "points" => encodeData data.points
+        ]
+
+
+encodeData data =
+    Encode.list <| encodePoints data
+
+
+encodePoints points =
+    List.map encodePoint points
+
+
+encodePoint =
+    tuple2Encoder Encode.string Encode.int
+
+
+tuple2Encoder : (a -> Value) -> (b -> Value) -> ( a, b ) -> Value
+tuple2Encoder enc1 enc2 ( val1, val2 ) =
+    Encode.list [ enc1 val1, enc2 val2 ]
